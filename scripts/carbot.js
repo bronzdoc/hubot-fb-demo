1724d16fd1be2d1ed31980da537327207ed80e4d
bronzdoc <lsagastume1990@gmail.com>
2016-04-21 21:24:40 -0600

    Changed bot logic

--- scripts/carbot.js
+++ scripts/carbot.js
@@ -1,22 +1,22 @@
 module.exports = function(robot) {
-    robot.hear(/getting chilly/i, function(res) {
+    robot.hear(/millionare/i, function(res) {
         res.envelope.fb = {
             richMsg: {
                 attachment: {
                     type: "template",
                     payload: {
                         template_type: "button",
-                        text: "Do you wanna build a snowman?",
+                        text: "Do you wanna be millionare? ",
                         buttons: [
                         {
                             type: "web_url",
-                            url: "http://www.dailymotion.com/video/x1fa7w8_frozen-do-you-wanna-build-the-snowman-1080p-official-hd-music-video_music",
+                            url: "http://i.imgur.com/dPzC1o6.gif",
                             title: "Yes"
                         },
                         {
                             type: "web_url",
+                            url: "http://i0.kym-cdn.com/photos/images/original/000/593/159/ee6.gif"
                             title: "No",
-                            url: "http://wallpaper.ultradownloads.com.br/275633_Papel-de-Parede-Meme-Okay-Face_1600x1200.jpg"
                         }
                         ]
                     }
